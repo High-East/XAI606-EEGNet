7725cb40cb735bfbd3df3a9834f541a485a8d42e
High-East <rhehd127@gmail.com>
2021-10-24 00:13:38 +0900

    [Update] Add random seed

--- runs/train.sh
+++ runs/train.sh
@@ -1,1 +1,1 @@
-python main.py --all_subject --summary --model=EEGNet --batch_size=72 --epochs=1000 -lr=2e-03 -wd=2e-03 -sch=cosine --save_dir=train  --device=0
+python main.py --all_subject --summary --model=EEGNet --batch_size=72 --epochs=1000 -lr=2e-03 -wd=2e-03 -sch=cosine --save_dir=train  --device=0 --seed=1207
